a6cac9640d833b40e20d7d4c979c8f3ee2dae0f8
Trinity Nguyen <thn.trinity@gmail.com>
2021-04-29 20:50:01 -0700

    Continuous integration and deployment with AWS

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello</p>
+        <p>I was changed on the Feature branch</p>
         <a
           className="App-link"
           href="https://reactjs.org"
